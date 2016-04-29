e3d9c5e3edac796312e74969ccf6498d7fbb0da9
Jacob <jaochs7@gmail.com>
2016-04-28 20:19:09 -0700

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+m1 = Message.create(content: "This message is a previously seeded example")
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
